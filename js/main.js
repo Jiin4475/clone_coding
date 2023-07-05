6e0e2e6296f9eb2a303373aa629dd32228e807d9
Jiin <yujiin4475@gmail.com>
2023-07-05 21:35:40 +0900

    scroll
    acroll 섹션 정리

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 // 전역변수를 피하기 위해,즉시 호출 함수 (function (){})
 //4개의 객체를 만듬 - 4개 구간으로 구성되어있어서
 //애니메이션 구간 설정 (scrollHeight)
+
+
 (() => {
+ let yOffset = 0;//window.pageYOffset 대신 쓸 변수
+ let prevScrollHeight = 0;//현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+ let currentScene = 0;//현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+
  const sceneInfo = [
-    {
-        //0
+    { //0
     type:'sticky',
     heightNum: 5,// 브라우저 높이의 5배로 scrollHeight 셋팅
     scrollHeight:0,
@@ -51,10 +56,35 @@
         sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
         sceneInfo[i].objs.container.style.height = `${ sceneInfo[i].scrollHeight}px`;
     }
+    
  }
 
- window.addEventListener('resize',setLayout);
- //윈도우창의 사이즈가 변할 때 add event 주고, setLayout 실행
 
+ function scrollLoop(){
+    prevScrollHeight= 0;//값이 누적이 되지 않게 0
+   for( let i = 0; i < currentScene; i++){
+    prevScrollHeight += sceneInfo[i].scrollHeight;
+
+   }
+   if(yOffset > prevScrollHeight+ sceneInfo[currentScene].scrollHeight){
+    currentScene++;
+   }
+   if(yOffset < prevScrollHeight){
+    if(currentScene === 0) return;
+    // 종종 다른 브라우저에서 스크롤 바운싱 될 때 마이너스로 취급 될 수 있기 때문에 그러면 에러가 발생 할 수 있음 그걸 방지하기 위해서  if(currentScene === 0) return; 주면 return 종료 
+  
+    currentScene--;
+   }
+   console.log(currentScene);
+
+}
+window.addEventListener('resize',setLayout);
+ //윈도우창의 사이즈가 변할 때 add event 주고, setLayout 실행
+window.addEventListener('scroll',() => {
+    yOffset = window.pageYOffset;
+      scrollLoop();
+});
+
+ 
  setLayout();//실행
 })();
