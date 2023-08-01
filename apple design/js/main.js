a73ccba48dd32ec5c89d98187733c19fbe824548
Jiin <yujiin4475@gmail.com>
2023-08-01 12:55:10 +0900

    .

--- apple design/js/main.js
+++ apple design/js/main.js
@@ -22,8 +22,12 @@ const sceneInfo = [
             ('#scroll-section-0 .main-messgae.d'),
         },
         values:{// 애니메이션 구간설정 start 0.1 end 0.2
-            messageA_opacity: [0, 1, { start:0.1, end:0.2 }],//0~1, 
-            messageB_opacity: [0, 1, { start:0.3, end:0.4 }],
+            messageA_opacity_in: [0, 1, { start:0.1, end:0.2 }],//0~1, 
+            //messageB_opacity_in: [0, 1, { start:0.3, end:0.4 }],
+            messageA_translateY_in: [20, 0, { start:0.1, end:0.2 } ],
+
+            messageA_opacity_out: [1, 0, { start:0.25, end:0.3 }],
+            messageA_translateY_out: [0, -20, { start:0.25, end:0.3 } ],
         }
     },
     { // 1
@@ -104,15 +108,30 @@ const sceneInfo = [
         const objs = sceneInfo[currentScene].objs;
         const values = sceneInfo[currentScene].values;
         const currentYOffset = yOffset - prevScrollHeight;
-        console.log(currentScene);
+        const scrollHeight =sceneInfo[currentScene].scrollHeight
+        const scrollRatio = (yOffset - prevScrollHeight) / scrollHeight;
+   
        
     switch (currentScene) {
-        case 0:
+        case 0: 
             //console.log('0 play');
-            let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffset);
+            const messageA_opacity_in = calcValues(values.messageA_opacity_in, currentYOffset);
+            const messageA_opacity_out = calcValues(values.messageA_opacity_out, currentYOffset);
+
+            const messageA_translateY_in = calcValues(values.messageA_translateY_in, currentYOffset);
+            const messageA_translateY_out = calcValues(values.messageA_translateY_out, currentYOffset);
+            
+            
+            if(scrollRatio <= 0.22 ) {
+                // in
+                objs.messageA.style.opacity = messageA_opacity_in;
+                objs.messageA.style.transform = `translateY(${messageA_translateY_in}%)`;
+            }else {
+                //out
+                objs.messageA.style.opacity = messageA_opacity_out;
+                objs.messageA.style.transform = `translateY(${messageA_translateY_out}%)`;
+            }
             //스크롤 값에 맞게 투명=> 진해졌다가=>
-            objs.messageA.style.opacity = messageA_opacity_in;
-            console.log(messageA_opacity_in);
             break;
 
         case 1:
